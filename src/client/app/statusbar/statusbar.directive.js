d697c1abb932a0362f0b7ba6796d30c78197dc10
steffennilsen <steffennilsen@gmail.com>
2015-04-10 23:47:27 +0200

    wip

---
+++ src/client/app/statusbar/statusbar.directive.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('statusbar')
+        .directive('statusbar', statusBar);
+
+    function statusBar() {
+        var directive = {
+            restrict: 'EA',
+            templateUrl: 'app/statusbar/statusbar.directive.html',
+            scope: {
+                max: '='
+            },
+            link: linkFunc,
+            controller: Controller,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+
+        return directive;
+
+        function linkFunc(scope, el, attr, ctrl) {
+        }
+    }
+
+    function Controller() {
+        var vm = this;
+    }
+})();
